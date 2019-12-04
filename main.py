0226a51a4d1e060c7995f9c83cbb7f2dce387a7b
satory074 <satory074@gmail.com>
2019-12-04 15:03:55 +0900

    refactor: url2json
    - urlからjsonファイルに再保存する処理を関数化

--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import json
 import urllib.parse
 import myutil
 
+fileio = myutil.FileIO()
+
+
+def url2json(url:str):
+    json_ = json.loads(urllib.parse.unquote(url).split("json=")[1])
+    fileio.write(data=json_, path=f"paifu/json/{'-'.join(map(str, json_['log'][0][0][:2]))}.json")
+
 
 def main():
-    fileio = myutil.FileIO()
-    for i, s in enumerate(fileio.read(path="paifu/txt/2019093001.txt")):
-        json_ = json.loads(urllib.parse.unquote(s).split("json=")[1])
-        fileio.write(data=json_, path=f"paifu/json/{'-'.join(map(str, json_['log'][0][0][:2]))}.json")
+    (url2json(url=s) for s in fileio.read(path="paifu/txt/2019093001.txt"))
+
 
 
 if __name__ == '__main__':
