e737e5342afc4a39a020ead1ff93dad9fe00d22a
satory074 <satory074@gmail.com>
2019-12-04 17:32:57 +0900

    unbobo

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import glob
 import json
 import urllib.parse
 import myutil
+from typing import List
 
 fileio = myutil.FileIO()
 
+
+class Player:
+    def __init__(self, haipai:List[int], tsumopai:List[int], sutepai:List[int]):
+        self.haipai:List[int] = haipai
+        self.tsumopai:List[int] = tsumopai
+        self.sutepai:List[int] = sutepai
 
 def url2json(url:str):
     json_ = json.loads(urllib.parse.unquote(url).split("json=")[1])
@@ -13,6 +20,10 @@ def url2json(url:str):
 
 def main():
     (url2json(url=s) for s in fileio.read(path="paifu/txt/2019093001.txt"))
+
+    for path in glob.glob('paifu/json/*-*.json'):
+        print(fileio.read(path=path))
+        exit()
 
 
 
--- myutil.py
+++ myutil.py
@@ -5,9 +5,15 @@ from typing import List
 
 class FileIO:
     @staticmethod
-    def read(path: str) -> List[str]:
-        with open(path, mode='r') as f:
-            return f.readlines()
+    def read(path: str):
+        _, ext = os.path.splitext(path)
+
+        if ext == '.txt':
+            with open(path, mode='r') as f:
+                return f.readlines()
+        if ext == '.json':
+            with open(path) as f:
+                return json.load(f)
 
     @staticmethod
     def output(li: List[str], path: str):
@@ -18,8 +24,8 @@ class FileIO:
     def write(data:str, path:str, mode_:str="w"):
         _, ext = os.path.splitext(path)
 
-        if ext == ".txt":
+        if ext == '.txt':
             with open(path, mode=mode_) as f:
                 print(data, file=f)
-        if ext == ".json":
+        if ext == '.json':
             json.dump(data, open(path, 'w') , ensure_ascii=False)
