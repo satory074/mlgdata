2434ecb9354586b46b01260ef9c3db1d984d3482
satory074 <satory074@gmail.com>
2020-11-07 10:52:03 +0900

    unbobo

--- main.py
+++ main.py
@@ -16,58 +16,71 @@ class Player:
         self.sutepai: List[int] = sutepai
 
 def reformat_json(json_: dict):
-        # log
-        di_kyoku = {
-            'nkyoku': json_['log'][0][0][0],
-            'nhonba': json_['log'][0][0][1],
-            'nkyotaku': json_['log'][0][0][2],
-            'dora_omote': json_['log'][0][2],
-            'dora_ura': json_['log'][0][3],
-            'result': json_['log'][0][-1][0],
-            'hora': '',
-            'hoju': '',
-            'nazo': '',  # kore ha nani?
-            'tensu': '',
-            'yaku': []
-            }
+    """
+    天鳳牌譜JSONをreformat
 
-        if di_kyoku['result'] == '和了':
-            di_kyoku['hora'] = json_["name"][json_["log"][0][-1][2][0]]
-            di_kyoku['hoju'] = json_["name"][json_["log"][0][-1][2][1]] if json_["log"][0][-1][2][0] != json_["log"][0][-1][2][1] else ''
-            di_kyoku['nazo'] = json_["log"][0][-1][2][2]
-            di_kyoku['tensu'] = json_["log"][0][-1][2][3]
-            di_kyoku['yaku'] = json_["log"][0][-1][2][4:]
+    @args: dict | 天鳳牌譜URLを変換した天鳳牌譜JSON
+    @return: dict | reformatした天鳳牌譜JSON
+    """
 
-        di_player = {
-            json_['name'][i].split('（')[0]: {
-                'team': json_['name'][i][-3:-1],
-                'mochiten': json_['log'][0][1][i],
-                'shushi':  json_['log'][0][-1][1][i],
-                'haipai': json_['log'][0][3*i+4],
-                'tsumopai': json_['log'][0][3*i+5],
-                'sutepai': json_['log'][0][3*i+6]
-            } for i in range(4)
+    # log
+    di_kyoku = {
+        'nkyoku': json_['log'][0][0][0],
+        'nhonba': json_['log'][0][0][1],
+        'nkyotaku': json_['log'][0][0][2],
+        'dora_omote': json_['log'][0][2],
+        'dora_ura': json_['log'][0][3],
+        'result': json_['log'][0][-1][0],
+        'hora': '',
+        'hoju': '',
+        'nazo': '',  # kore ha nani?
+        'tensu': '',
+        'yaku': []
         }
 
-        json_['log'] = {
-            'kyoku': di_kyoku,
-            'player': di_player
-            }
+    if di_kyoku['result'] == '和了':
+        di_kyoku['hora'] = json_["name"][json_["log"][0][-1][2][0]]
+        di_kyoku['hoju'] = json_["name"][json_["log"][0][-1][2][1]] if json_["log"][0][-1][2][0] != json_["log"][0][-1][2][1] else ''
+        di_kyoku['nazo'] = json_["log"][0][-1][2][2]
+        di_kyoku['tensu'] = json_["log"][0][-1][2][3]
+        di_kyoku['yaku'] = json_["log"][0][-1][2][4:]
 
-        return json_
+    di_player = {
+        json_['name'][i].split('（')[0]: {
+            'team': json_['name'][i][-3:-1],
+            'mochiten': json_['log'][0][1][i],
+            'shushi':  json_['log'][0][-1][1][i],
+            'haipai': json_['log'][0][3*i+4],
+            'tsumopai': json_['log'][0][3*i+5],
+            'sutepai': json_['log'][0][3*i+6]
+        } for i in range(4)
+    }
+
+    json_['log'] = {
+        'kyoku': di_kyoku,
+        'player': di_player
+        }
+
+    return json_
 
 def url2json(url: str):
-    json_ = json.loads(urllib.parse.unquote(url).split('json=')[1])
-    json_ = reformat_json(json_)
+    """
+    天鳳牌譜URLをJSON形式で再保存
 
-    fileio.write(data=json_, path=f"paifu/json/{json_['log']['kyoku']['nkyoku']}-{json_['log']['kyoku']['nhonba']}.json")
+    @args: url | tenhou paifu url
+    """
+    json_ = json.loads(urllib.parse.unquote(url).split('json=')[1])
+    fileio.write(data=json_, path=f"paifu/json/{json_['log'][0][0][0]}-{json_['log'][0][0][1]}.json")
+
+    json_fmt = reformat_json(json_)
+    fileio.write(data=json_, path=f"paifu/json-format/{json_['log']['kyoku']['nkyoku']}-{json_['log']['kyoku']['nhonba']}.json")
 
 
 def main():
     [url2json(url=s) for s in fileio.read(path="paifu/txt/2019093001.txt")]
 
-    for path in glob.glob('paifu/json/*-*.json'):
-        print(fileio.read(path=path))
+    # for path in glob.glob('paifu/json/*-*.json'):
+    #     print(fileio.read(path=path))
 
 
 if __name__ == '__main__':
